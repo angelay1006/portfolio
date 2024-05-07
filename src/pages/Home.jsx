81d05d85ed97f16c6a20461ee26dea5346f5a41e
angelay1006 <angela_yeung@brown.edu>
2024-05-07 00:21:28 -0400

    homepage done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,39 +1,23 @@
-// import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './Home.css';
-
-// const Home = () => {
-//     return (
-//         <div className="home-container d-flex justify-content-center align-items-center">
-//             <div className="row w-100">
-//                 <div className="col-12 col-lg-10 offset-lg-1">
-//                     <div className="text-center title-wrapper">
-//                         <h1 className="display-2">Hi. I'm Angela Yeung</h1>
-//                         <p className="subtitle text-start">I’m passionate about developing environmentally and socially responsible computing solutions.</p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Home;
-
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Home.css';
+import geometry from '../assets/geometry.svg';
+
+
+
 
 const Home = () => {
     return (
-        <div className="home-container d-flex justify-content-center align-items-center">
-            <div className="title-container">
-                <h1 className="display-2">Hi. I'm Angela Yeung</h1>
+        <div className="container-fluid home-container d-flex justify-content-center align-items-center">
+            <div className="row">
+                <div className="col-12 text-center">
+                    <h1 className="display-2">Hi. I'm Angela Yeung</h1>
                     <p className="subtitle">I’m passionate about developing environmentally and socially responsible computing solutions.</p>
                 </div>
+            </div>
         </div>
     );
 }
 
 export default Home;
-
 
