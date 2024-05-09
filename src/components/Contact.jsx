e178b33160dd192a2d4452b60fb4252840da5924
angelay1006 <angela_yeung@brown.edu>
2024-05-09 19:15:23 -0400

    update> about me

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,8 +22,8 @@ const Contact = () => {
                     <div class="col-md-8">
                         <div class="card-body">
                             <h5 class="card-title"> About Me </h5>
-                            <p class="card-text">Hello! Based in Providence, Rhode Island, I am soon to graduate from Brown University with a Bachelor of Science, where I concentrated on artificial intelligence and security. I am excited to further my expertise in these fields by pursuing a master's program at Brown. </p>
-                            <p class="card-text">Outside of software and academics, I love reading contemporary fiction and fantasy literature, gaming, and playing classical music. </p>
+                            <p class="card-text">Hello! Based in Providence, Rhode Island, I am soon to graduate from Brown University with a Bachelor of Science degree, where I concentrated in Artificial Intelligence and Security. I am excited to further my expertise in these fields by pursuing a master's degree at Brown. </p>
+                            <p class="card-text">Outside of software, I love reading contemporary fiction and fantasy literature, gaming, and playing classical music. </p>
                         </div>
                     </div>
                 </div>
